7ff900e5b09d255dde4cacf7a1d6a9aa4f57c021
prakharmartand@prophecy.io <prakharmartand@prophecy.io>
2025-12-10 15:12:32 +0000

    Remove `user_order_aggregate` transformation from `pipeline1` pipeline

--- HelloSQL/pipelines/pipeline1.py
+++ HelloSQL/pipelines/pipeline1.py
@@ -1,25 +1,6 @@
 with DAG():
-    pipeline1__user_order_aggregate = Task(
-        task_id = "pipeline1__user_order_aggregate", 
-        component = "Model", 
-        modelName = "pipeline1__user_order_aggregate"
-    )
-    raw_customers = Task(
-        task_id = "raw_customers", 
-        component = "Dataset", 
-        writeOptions = {"writeMode" : "overwrite"}, 
-        table = {"name" : "raw_customers", "sourceType" : "Seed", "alias" : ""}
-    )
     pipeline1__season_years = Task(
         task_id = "pipeline1__season_years", 
         component = "Model", 
         modelName = "pipeline1__season_years"
     )
-    raw_orders = Task(
-        task_id = "raw_orders", 
-        component = "Dataset", 
-        writeOptions = {"writeMode" : "overwrite"}, 
-        table = {"name" : "raw_orders", "sourceType" : "Seed", "alias" : ""}
-    )
-    raw_orders.out >> pipeline1__user_order_aggregate.in_0
-    raw_customers.out >> pipeline1__user_order_aggregate.in_1
